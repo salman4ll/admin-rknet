cd065e8830a65051c422ed7ac73b51c09841ade9
salman4ll <sfarisi0101@gmail.com>
2023-06-06 15:04:23 +0700

    commit ke-5

--- js/produk.js
+++ js/produk.js
@@ -18,6 +18,7 @@ $.ajax(
                         '<td>'+data.title+'</td>'+
                         '<td>'+data.deskripsi+'</td>'+
                         '<td>'+data.harga+'</td>'+
+                        '<td>'+data.bandwidth+'</td>'+
                         '<td><button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button> &nbsp; <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button></td>'+
                     '</tr>'    
                 );
@@ -27,5 +28,33 @@ $.ajax(
         error: (err)=>{
             console.log(err);
         }
+    }
+);
+
+$('#form').submit(
+    ()=>{
+       $.ajax(
+        {
+         url:'https://ap-southeast-1.aws.data.mongodb-api.com/app/rk-net-wjgwl/endpoint/produk',
+         type:'POST',
+         data:JSON.stringify({ title:$('#titlenya').val(),
+         deskripsi:$('#deskripsinya').val(),
+         harga:$('#harganya').val(),
+         bandwidth:$('#bandwidthnya').val(),
+         }),
+         contentType: 'application/json',
+         beforeSend:()=>{$('#form button').prop('disabled',true);},
+         success:(res)=>{
+                      $('#form button').prop('disabled',false);
+                      window.location="produk.html";
+                   },
+         error:(err)=>{
+                    $('#form button').prop('disabled',false);
+                    console.log(err.message);	
+                    console.log($('#harganya').val())
+                }
+        }
+       );
+       return false;
     }
 );
